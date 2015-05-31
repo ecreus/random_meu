0720fd60b28c07292271d551d8abe195901932e8
Emili Creus <emili.creus@gmail.com>
2015-05-31 16:05:50 +0200

    archivo nuevo
    archivo nuevo

---
+++ random.js
@@ -0,0 +1,7 @@
+// MAth.random() devuelve un numero aleatorio entre 0 i 1
+var numero = Math.random();
+if (numero<= 0.5){
+ console.log('\n' + numero + ' MENOR qu 0,5 \n');
+}else{
+ console.log('\n' + numero + ' MAYOR qu 0,5 \n');
+}
